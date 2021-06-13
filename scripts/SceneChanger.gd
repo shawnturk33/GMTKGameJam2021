6b87c8271d7ee43a22d60c1b64db59263956f895
Shawn Turk <shawnturk33@gmail.com>
2021-06-13 10:32:26 -0400

    Release build.

--- scripts/SceneChanger.gd
+++ scripts/SceneChanger.gd
@@ -10,6 +10,6 @@ func change_scene(path, delay = 0.5):
 	yield(get_tree().create_timer(delay), "timeout")
 	animationPlayer.play("Fade")
 	yield(animationPlayer, "animation_finished")
-	assert(get_tree().change_scene(path) == OK)
+	get_tree().change_scene(path)
 	animationPlayer.play_backwards("Fade")
 	emit_signal("scene_changed")
